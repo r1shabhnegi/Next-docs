5f060acbb474085bdfdac632d6ade693317c44d3
irishabhnegi <rishabhnegi175@gmail.com>
2024-01-16 19:49:29 +0530

    fetch data (server side)

--- app/data/page.jsx
+++ app/data/page.jsx
@@ -1,31 +1,18 @@
-'use client';
+async function fetchData() {
+  let res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  let data = await res.json();
+  return data;
+}
 
-import { useEffect, useState } from 'react';
-
-const Data = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const data = await res.json();
-      setProducts(data);
-      console.log(data);
-    }
-
-    fetchData();
-  }, []);
-
+const Data = async () => {
+  let products = await fetchData();
+  console.log(products);
   return (
-    <ul>
+    <div>
       {products?.map((p) => (
-        <li
-          className='border px-2 py-2'
-          key={p.id}>
-          {p.title}
-        </li>
+        <p key={p.id}>{p.title}</p>
       ))}
-    </ul>
+    </div>
   );
 };
 export default Data;
