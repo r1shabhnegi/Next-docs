63a397fe38b3c9358c4d27817b4ac4c6af309de7
irishabhnegi <rishabhnegi175@gmail.com>
2024-01-16 19:18:38 +0530

    fonts

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-
+import { Poppins } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,20 +1,12 @@
-import img from '../../public/img.jpg';
-import Image from 'next/image';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({ subsets: ['latin'], weight: '500' });
+
 const Users = () => {
   return (
     <div>
-      <Image
-        src={img}
-        alt='image'
-        width={700}
-        height={700}
-      />
-      <Image
-        src='https://images.unsplash.com/photo-1682686581312-506a8b53190e?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        alt='nature-img'
-        width='1000'
-        height='1000'
-      />
+      <h1>Google fonts</h1>
+      <h2 className={poppins.className}>Hello this is a poppins</h2>
     </div>
   );
 };
