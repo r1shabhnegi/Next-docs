7c9e81deb008914981216cc440538c00110ecabc
irishabhnegi <rishabhnegi175@gmail.com>
2024-01-16 22:46:38 +0530

    env

--- app/api/users/route.jsx
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-import { users } from '../../util/db';
-export function GET() {
-  const data = users;
-  return NextResponse.json({ data });
-}
---
+++ app/random/page.jsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  console.log(process.env.MONGO_URI);
+  return <div>Page</div>;
+};
+export default Page;
