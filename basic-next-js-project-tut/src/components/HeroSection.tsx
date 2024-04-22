2fcae4c7c9769eaf90b93207f438b7c92b61e4cc
irishabhnegi <rishabhnegi175@gmail.com>
2024-04-22 14:11:20 +0530

    basic-tut-final

--- basic-next-js-project-tut/src/components/HeroSection.tsx
+++ basic-next-js-project-tut/src/components/HeroSection.tsx
@@ -1,8 +1,18 @@
 import Link from 'next/link';
+import { cn } from '@/utils/cn';
+import { Spotlight } from './ui/Spotlight';
+import { Button } from './ui/moving-border';
 
 const HeroSection = () => {
   return (
     <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
+      <Spotlight
+        className='-top-40 left-30 md:left-80 md:-top-20'
+        fill='white'
+      />
+      {/* <Spotlight */}
+      {/* className='-top-40 left-30 md:left-80 md:-top-20'
+        fill='white'> */}
       <div className='p-4 relative z-10 w-full text-center'>
         <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>
           Master the art of music
@@ -12,7 +22,13 @@ const HeroSection = () => {
           journey today. Whether you're a beginner or looking to refine your
           skills, join us to unlock your true potential.
           <div className='mt-4'>
-            <Link href={'/courses'}>Explore courses</Link>
+            <Link href={'/courses'}>
+              <Button
+                borderRadius='1.75rem'
+                className='bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800'>
+                Explore courses
+              </Button>
+            </Link>
           </div>
         </p>
       </div>
